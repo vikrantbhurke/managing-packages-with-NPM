08fd69b91ccef4e7a5590c711fe1fc265b8e8841
Vikrant Bhurke <96013855+vikrantbhurke@users.noreply.github.com>
2022-06-10 11:49:17 +0530

    Added semicolon to myApp

--- myApp.js
+++ myApp.js
@@ -2,5 +2,5 @@ let express = require('express');
 var bGround = require('fcc-express-bground');
 let app = express();
  module.exports = app;
-bGround.log("Hello World")
- console.log("Hello World")
+bGround.log("Hello World");
+ console.log("Hello World");
