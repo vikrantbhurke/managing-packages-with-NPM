a379a86b522f28d531b56669e56c67ade3be636c
Vikrant Bhurke <96013855+vikrantbhurke@users.noreply.github.com>
2022-06-10 11:47:03 +0530

    Added console.log

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
+var bGround = require('fcc-express-bground');
 let app = express();
  module.exports = app;
-
+bGround.log("Hello World")
  console.log("Hello World")
